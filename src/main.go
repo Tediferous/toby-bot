e06cb26c4837c3e9d6922daef86203ecb2178a6b
Ted Dorfeuille <teddorfeuille@gmail.com>
2020-07-11 11:23:43 -0400

    test(create,delete): handlers and helper functions

--- src/main.go
+++ src/main.go
@@ -7,6 +7,8 @@ import (
         "os"
         "os/signal"
         "syscall"
+        "encoding/json"
+        //"time"
 )
 
 // Variables used for command line parameters
@@ -14,6 +16,7 @@ var (
 	Token string
         Spam string
 )
+
 
 func init() {
 
@@ -30,6 +33,7 @@ func main(){
         Check(err)
 
         dg.AddHandler(messageCreate)
+        dg.AddHandler(messageDelete)
 
         //Open socket
         Check(dg.Open())
@@ -50,16 +54,62 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
+        trace(s)
+        trace(m)
 
+        // general debug and joke messages
+        switch m.Content {
+        case "ping":
+                s.ChannelMessageSend(Spam, "Pong!")
+                return
         // :crown:
-	if m.Content == "who's daddy" {
+        case "who's daddy":
                 s.ChannelMessageSend(Spam, "You are, King.")
-	}
+                return
+        }
 
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(Spam, "Pong!")
-	}
+        // if m.Mentions
+
+}
+
+func messageDelete(s *discordgo.Session, m *discordgo.MessageDelete) {
+        // TODO: make this function do something
+
+        trace(s)
+        trace(m)
+	// // Ignore all messages created by the bot itself
+	// if m.Message.Author.ID == s.State.User.ID {
+	// 	return
+	// }
+
+        // s.ChannelMessageSend(Spam, "@"+m.Message.Author.Username +" deleted the message:\n"+"`"+m.Message.Content+"`")
+}
+
+func ban(warrant ...string){
+        // MemberID, TimeInterval
+        // member := warrant[0]
+        // sentence := warrant[1]
+        // roles := member.Roles
+
+        //remove all roles from member
+
+        //add banned role to member
+
+        //parse time and sleep
+
+        //remove banned role, add old roles
+}
+
+func poll(prompt string, quorum int){
+        //send message to channel with prompt
+        // options := map[string]int{
+        //         ":thumbsup:": 1,
+        //         ":thumbsdown:": 0,
+        // }
+
+        //watch message reactions
+                //if either reaction reaches quorum
+                //return options[reaction]
 }
 
 func Check(err error){
@@ -69,3 +119,7 @@ func Check(err error){
         }
 }
 
+func trace(s interface{}){
+        res, _ := json.MarshalIndent(s,"","  ")
+        log.Info(string(res))
+}
